bc9a9691c188c081320f603006d4334c14c389b6
David Kwak <kwak27@illinois.edu>
2013-05-29 14:08:51 -0700

    added full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,14 @@ describe "Static pages" do
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Sample App | Home")
+			page.should have_selector('title', :text => "Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
 
@@ -38,6 +43,19 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
 														:text => "Sample App | About Us")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+														:text => "Sample App | Contact")
 		end
 	end
 
